ad7c07c22ac6a66891c885a560fb65a35e9f2abe
karimovdo <116346799+karimovdo@users.noreply.github.com>
2025-06-18 02:14:54 +0500

    12

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ min_nonmiss = st.slider("Мин. заполненность признака", 0
 min_row_fill = st.slider("Мин. заполненность строки (для каждого района)", 0.1, 0.9, 0.3, 0.05)
 test_years  = st.slider("Лет в тесте", 1, 3, 1)
 max_depth   = st.slider("max_depth", 2, 10, 6)
-n_estim     = st.slider("n_estimators", 100, 2000, 1000, 100)
+n_estim     = st.slider("n_estimators", 100, 1000, 1000, 50)
 
 if not st.button("🚀 Запустить анализ"):
     st.stop()
